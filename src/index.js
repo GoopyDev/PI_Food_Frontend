57180cd077ca62f9513d1f41f39e8f2a0b0e2dd5
GoopyDev <alann.romero@gmail.com>
2023-05-31 16:42:24 -0300

    Añadido axios.defaults.baseURL con la direccion del servidor desplegado en la web

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import axios from "axios";
+
+axios.defaults.baseURL = "goopy-foodserver.up.railway.app";
 
 ReactDOM.render(
   <Provider store={store}>
