5eeef0599db6c5151d240383be6a9ced3091697c
GoopyDev <alann.romero@gmail.com>
2023-05-31 16:56:55 -0300

    Corrección de la URL del servidor

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-axios.defaults.baseURL = "goopy-foodserver.up.railway.app";
+axios.defaults.baseURL = "https://goopy-foodserver.up.railway.app";
 
 ReactDOM.render(
   <Provider store={store}>
